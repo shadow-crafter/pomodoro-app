1fd1173ba03c3b3267a426db840f28108f1c7d5a
shadowcrafter <65373035+shadow-crafter@users.noreply.github.com>
2025-09-01 16:52:53 -0400

    Separate Timer class into src

--- main.py
+++ main.py
@@ -1,38 +1,5 @@
-import time
+from src.timer import Timer
 from tkinter import Tk, Frame, Button
-
-timer_states = {"tomato": 25, "break": 5, "long_break": 15}
-
-class Timer:
-    current_state = "tomato"
-    current_time = -1
-    tomatos = 0
-    paused = False
-
-    def __init__(self) -> None:
-        pass
-
-    def run(self) -> None:
-        self.current_time = timer_states[self.current_state]
-        while self.current_time > 0:
-            if self.paused:
-                print("Timer paused...")
-            print(f"Time left: {self.current_time}")
-            self.current_time -= 1
-            time.sleep(1)
-        self.tomatos += 1
-        self.switch_state()
-    
-    def switch_state(self) -> None:
-        if self.current_state == "tomato":
-            if self.tomatos % 4 == 0 and self.tomatos != 0:
-                self.current_state = "long_break"
-                self.tomatos = 0
-            else:
-                self.current_state = "break"
-        elif self.current_state == "break" or self.current_state == "long_break":
-            self.current_state = "tomato"
-        self.run()
 
 def create_window() -> None:
     root: Tk = Tk()
